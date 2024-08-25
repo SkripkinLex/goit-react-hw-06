3d88bb07df4b9cf5f61e476a4c01db91ca266433
Oleksii Bespalyi <skripkinlexxx@gmail.com>
2024-08-25 21:31:53 +0200

    add file

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const INITIAL_STATE = {
+  items: [],
+};
+
+const contactsSlice = createSlice({
+  name: "items",
+  initialState: INITIAL_STATE,
+  reducers: {
+    addContact: (state, action) => {
+      state.items.push(action.payload);
+    },
+    deleteContact: (state, action) => {
+      state.items = state.items.filter((item) => item.id !== action.payload);
+    },
+  },
+});
+
+export const contactsReducer = contactsSlice.reducer;
+export const { addContact, deleteContact } = contactsSlice.actions;
